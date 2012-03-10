48238c482762f6fce66b1ca298194f44f8dfa076
Federico Alcantara <falcantara@git.code.sf.net>
2012-03-10 22:12:21 +0000

    -wubiq-common. Implemented 98% of Graphics2D commands. Pending setComposite.

---
+++ wubiq-common/src/net/sf/wubiq/wrappers/RenderedImageWrapper.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package net.sf.wubiq.wrappers;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
+import java.util.Hashtable;
+
+/**
+ * @author Federico Alcantara
+ *
+ */
+public class RenderedImageWrapper extends ImageWrapper {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Default constructor.
+	 */
+	public RenderedImageWrapper() {
+		super();
+	}
+	
+	/**
+	 * Prefered constructor.
+	 * @param img RenderedImage to be serialized.
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public RenderedImageWrapper(RenderedImage img) {
+		BufferedImage image = null;
+		if (img instanceof BufferedImage) {
+			image = (BufferedImage)img;	
+		} else {
+			ColorModel cm = img.getColorModel();
+			int width = img.getWidth();
+			int height = img.getHeight();
+			WritableRaster raster = cm.createCompatibleWritableRaster(width, height);
+			boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+			Hashtable properties = new Hashtable();
+			String[] keys = img.getPropertyNames();
+			if (keys!=null) {
+				for (int i = 0; i < keys.length; i++) {
+					properties.put(keys[i], img.getProperty(keys[i]));
+				}
+			}
+			image = new BufferedImage(cm, raster, isAlphaPremultiplied, properties);
+			img.copyData(raster);
+		}
+		serializeImage(image);
+	}
+	
+	public RenderedImage getRenderedImage(double xScale, double yScale) {
+		return (RenderedImage)((BufferedImage)super.getImage(xScale, yScale));
+	}
+}
---
+++ wubiq-common/src/net/sf/wubiq/wrappers/StrokeWrapper.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package net.sf.wubiq.wrappers;
+
+import java.awt.BasicStroke;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.io.Serializable;
+
+/**
+ * @author Federico Alcantara
+ *
+ */
+public class StrokeWrapper implements Stroke, Serializable {
+	private static final long serialVersionUID = 1L;
+	private float width;
+	private int cap;
+	private int join;
+	private float miterLimit;
+	private float[] dash;
+	private float dashPhase;
+	private boolean nullObject;
+	
+	public StrokeWrapper(){
+		nullObject = true;
+	}
+	
+	public StrokeWrapper(Stroke stroke) {
+		this();
+		if (stroke != null && stroke instanceof BasicStroke) {
+			BasicStroke basicStroke = (BasicStroke)stroke;
+			width = basicStroke.getLineWidth();
+			cap = basicStroke.getEndCap();
+			join = basicStroke.getLineJoin();
+			miterLimit = basicStroke.getMiterLimit();
+			dash = basicStroke.getDashArray();
+			dashPhase = basicStroke.getDashPhase();
+			nullObject = false;
+		}
+	}
+	/**
+	 * @return the width
+	 */
+	public float getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	/**
+	 * @return the cap
+	 */
+	public int getCap() {
+		return cap;
+	}
+	/**
+	 * @param cap the cap to set
+	 */
+	public void setCap(int cap) {
+		this.cap = cap;
+	}
+	/**
+	 * @return the join
+	 */
+	public int getJoin() {
+		return join;
+	}
+	/**
+	 * @param join the join to set
+	 */
+	public void setJoin(int join) {
+		this.join = join;
+	}
+	/**
+	 * @return the miterlimit
+	 */
+	public float getMiterLimit() {
+		return miterLimit;
+	}
+	/**
+	 * @param miterLimit the miterLimit to set
+	 */
+	public void setMiterlimit(float miterLimit) {
+		this.miterLimit = miterLimit;
+	}
+	/**
+	 * @return the dash
+	 */
+	public float[] getDash() {
+		return dash;
+	}
+	/**
+	 * @param dash the dash to set
+	 */
+	public void setDash(float[] dash) {
+		this.dash = dash;
+	}
+	/**
+	 * @return the dashPhase
+	 */
+	public float getDashPhase() {
+		return dashPhase;
+	}
+	/**
+	 * @param dashPhase the dashPhase to set
+	 */
+	public void setDashPhase(float dashPhase) {
+		this.dashPhase = dashPhase;
+	}
+	
+	public Stroke getStroke() {
+		if (!nullObject) {
+			return new BasicStroke(width, cap, join, miterLimit, dash, dashPhase);
+		}
+		return null;
+	}
+
+	@Override
+	public Shape createStrokedShape(Shape shape) {
+		Stroke stroke = getStroke();
+		if (stroke != null) {
+			return stroke.createStrokedShape(shape);
+		}
+		return null;
+	}
+	
+}
