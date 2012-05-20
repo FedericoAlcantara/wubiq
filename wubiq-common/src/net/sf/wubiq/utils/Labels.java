ded21d7a50b41d62ceec2cba9e3c5a12ce881018
Federico Alcantara <falcantara@git.code.sf.net>
2012-05-20 03:42:15 +0000

    -wubiq-common. Updated documentation.

--- wubiq-common/src/net/sf/wubiq/utils/Labels.java
+++ wubiq-common/src/net/sf/wubiq/utils/Labels.java
@@ -15,7 +15,7 @@ import org.apache.commons.logging.LogFactory;
  *
  */
 public class Labels {
-	public static final String VERSION = "1.0.233";
+	public static final String VERSION = "1.0.236";
 	private static final Log LOG = LogFactory.getLog(Labels.class);
 	private ResourceBundle bundle;
 	private String labelFile;
