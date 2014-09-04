88393dc4eb37dec9bcea4a515484ed2bb8264c64
Federico Alcantara <falcantara@sicflex.com>
2014-09-04 17:58:20 -0400

    -Added version info for version publication. -Unstable commit.

---
+++ wubiq-server/src/net/sf/wubiq/utils/VersionInfo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.sf.wubiq.utils;
+
+/**
+ * Just prints the version information.
+ * @author Federico Alcantara
+ *
+ */
+public class VersionInfo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Labels.VERSION);
+	}
+
+}
