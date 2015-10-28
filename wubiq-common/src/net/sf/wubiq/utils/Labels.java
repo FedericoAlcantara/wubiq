2c940507c0e1aa7735b328c698b31c530ba4a7a4
Federico Alcantara <falcantara@sicflex.com>
2015-10-28 17:09:57 -0400

    Preparing version 2.2

--- wubiq-common/src/net/sf/wubiq/utils/Labels.java
+++ wubiq-common/src/net/sf/wubiq/utils/Labels.java
@@ -16,7 +16,7 @@ import org.apache.commons.logging.LogFactory;
  *
  */
 public class Labels {
-	public static final String VERSION = "2.1.2";
+	public static final String VERSION = "2.2";
 	private static final Log LOG = LogFactory.getLog(Labels.class);
 	private ResourceBundle bundle;
 	private String labelFile;
