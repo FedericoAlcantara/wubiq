fc9ad512b00ffa106f6202e48ce6e3b6601ba487
Federico Alcantara <federico.alcantara@sicflex.com>
2024-09-21 09:12:01 -0400

    Improved bluetooth permission handling.

--- wubiq-android-gradle/app/build.gradle.kts
+++ wubiq-android-gradle/app/build.gradle.kts
@@ -8,10 +8,10 @@ android {
 
     defaultConfig {
         applicationId = "net.sf.wubiq.android"
-        minSdk = 22
+        minSdk = 28
         targetSdk = 34
-        versionCode = 51
-        versionName = "2.4.1"
+        versionCode = 54
+        versionName = "2.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -30,14 +30,14 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(files("libs/wubiq-client.jar"))
     implementation(files("libs/jsoup-1.9.1.jar"))
     implementation(files("libs/starioextension.jar"))
     implementation(files("libs/StarIOPort3.1.jar"))
     implementation(files("libs/ZSDK_API.jar"))
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
--- wubiq-android-gradle/app/src/main/java/net/sf/wubiq/android/ConfigureBluetoothActivity.java
+++ wubiq-android-gradle/app/src/main/java/net/sf/wubiq/android/ConfigureBluetoothActivity.java
@@ -5,8 +5,14 @@ package net.sf.wubiq.android;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.GridView;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 /**
  * Allows configuration of bluetooth devices.
@@ -14,7 +20,10 @@ import android.widget.GridView;
  *
  */
 public class ConfigureBluetoothActivity extends Activity {
-	
+	private static final int REQUEST_CODE = 2;
+
+	private SharedPreferences preferences = null;
+
 	/**
 	 * Called upon activity creation.
 	 */
@@ -22,16 +31,38 @@ public class ConfigureBluetoothActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.configure_bluetooth);
-		SharedPreferences preferences = getSharedPreferences(WubiqActivity.PREFERENCES, MODE_PRIVATE);
-		initialize(preferences);
+		preferences = getSharedPreferences(WubiqActivity.PREFERENCES, MODE_PRIVATE);
+		requestBluetooth();
 	}
-	
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		if (requestCode == REQUEST_CODE) {
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				initialize();
+			}
+		}
+	}
+
 	/**
 	 * Loads view with new or previously stored preferences.
-	 * @param preferences Application preferences.
 	 */
-	private void initialize(SharedPreferences preferences) {
+	private void initialize() {
 		GridView grid = (GridView) findViewById(R.id.devices);
 		grid.setAdapter(new BluetoothDeviceListAdapter(this, preferences));
+	}
+
+	private void requestBluetooth() {
+		boolean hasBluetooth = getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+
+		if (hasBluetooth) {
+			if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+					ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_CODE);
+				}
+			} else {
+				initialize();
+			}
+		}
 	}
 }
